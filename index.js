a9fe4651a643f8949e367b0eecbe2274b3ebf17b
ryanruri <muiruri13ryan@gmail.com>
2024-10-03 18:17:07 +0300

    initial git

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Your code here
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left < 360) { // Assuming the dodger can move up to 400px and is 40px wide
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+  
